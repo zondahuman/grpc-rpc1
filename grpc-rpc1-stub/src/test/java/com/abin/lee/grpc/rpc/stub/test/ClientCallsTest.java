11f2f6b8c63c645a6ce13e2541ffe8922c7dfc73
zondahuman <zondahuman@gmail.com>
2017-11-06 21:11:51 +0800

    add

--- grpc-rpc1-stub/src/test/java/com/abin/lee/grpc/rpc/stub/test/ClientCallsTest.java
+++ grpc-rpc1-stub/src/test/java/com/abin/lee/grpc/rpc/stub/test/ClientCallsTest.java
@@ -1,13 +1,10 @@
 package com.abin.lee.grpc.rpc.stub.test;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 /**
  * Created by abin on 2017/9/14 2017/9/14.
  * grpc-rpc1
  * com.abin.lee.grpc.rpc.stub.test
  */
-@RunWith(JUnit4.class)
 public class ClientCallsTest {
 
 
